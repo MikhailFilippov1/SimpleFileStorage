da80d6e8ca7ccb4ad87489021f6c4b59be82e948
MikhailFilippov1 <cp_filippov@mail.ru>
2022-06-12 16:40:34 +0300

    Добрый день. Прием от сервера к клиенту работает приемлемо (на больших файлах тоже). Отправка от клиента серверу проходит штатно, но прием на сервере барахлит. Куски файлов принимаются в неупорядоченном виде. Сначала может записать 10 кусков номер 1, потом тут же записать кусок номер 3 и тд и тп. Подскажите, что не так? Строка №47 в MainHandler.

--- Server/src/main/java/MainHandler.java
+++ Server/src/main/java/MainHandler.java
@@ -1,28 +1,81 @@
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
 
 public class MainHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        if (msg instanceof FileRequest) {
-            FileRequest fr = (FileRequest) msg;
-            if (Files.exists(Paths.get("server_storage/" + fr.getFilename()))) {
-                FileMessage fm = new FileMessage(Paths.get("server_storage/" + fr.getFilename()));
-                ctx.writeAndFlush(fm);
-            }
-        } else {
-            FileSend fs = (FileSend) msg;
-            FileMessage fm = new FileMessage(Paths.get(fs.getFilename()));
-            Files.write(Paths.get("server_storage/" + fm.getFilename()), fm.getData(), StandardOpenOption.CREATE);
-//            FileMessage fm = (FileMessage) msg;
-            System.out.println("File received>" + fs.getFilename());
-            System.out.println("Data>" + fm.getData());
+        if (msg instanceof FileRequest) {                                               //Отправка файла клиенту
+            new Thread(() -> {
+                try {
+                    FileRequest fr = (FileRequest) msg;
+                    File file = new File("server_storage/" + fr.getFilename());
+                    int bufSize = 1024 * 1024 * 1;
+                    int partsCount = new Long(file.length() / bufSize).intValue();
+                    if (file.length() % bufSize != 0) {
+                        partsCount++;
+                    }
+                    if (Files.exists(Paths.get("server_storage/" + fr.getFilename()))) {
+                        FileMessage fm = new FileMessage(Paths.get("server_storage/" + fr.getFilename()), -1, partsCount, new byte[bufSize]);
+                        FileInputStream in = new FileInputStream("server_storage/" + fr.getFilename());
+                        for (int i = 0; i < partsCount; i++) {
+                            int readedBytes = in.read(fm.data);
+                            fm.partNumber = i + 1;
+                            if (readedBytes < bufSize) {
+                                fm.data = Arrays.copyOfRange(fm.data, 0, readedBytes);
+                            }
+                            ctx.writeAndFlush(fm);
+                            Thread.sleep(100);
+                            System.out.println("Отправлена часть #" + (i + 1));
+                        }
+                        in.close();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+        if(msg instanceof FileMessage) {                                        //Прием файла от клиента
+            CountDownLatch cdl = new CountDownLatch(1);
+            Thread t = new Thread(() -> {
+            try {
+                FileMessage fs = (FileMessage) msg;
+                while (true) {
+//               AbstractMessage input = (FileMessage) msg; // Network.readObject();
+//                if(fs instanceof FileMessage) {
+//                    FileMessage fm = (FileMessage) input;
+                    boolean append = true;
+                    if (fs.partsCount == 1) {
+                        append = false;
+                    }
+                    System.out.println(fs.partNumber + " / " + fs.partsCount);
+                    FileOutputStream fos = new FileOutputStream("server_storage/" + fs.filename, append);
+                    fos.write(fs.data);
+                    fos.close();
+                    if (fs.partNumber == fs.partsCount) {
+                        cdl.countDown();
+                        break;
+                    }
+                }
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+            });
+            t.setDaemon(true);
+            t.start();
+
+
+//            FileMessage fs = (FileMessage) msg;
+//            Files.write(Paths.get("server_storage/" + fs.getFilename()), fs.getData(), StandardOpenOption.CREATE);
         }
     }
 
